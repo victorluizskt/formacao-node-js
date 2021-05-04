9011e46402563123eec4c1eba602cf4b1eaec1ed
Victor Luiz <victorluizcefet@gmail.com>
2021-05-04 20:15:28 -0300

    wip: hash de senha

--- Blog/user/UsersController.js
+++ Blog/user/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcryptjs = require("bcryptjs");
 
 router.get("/admin/users", (request, response) => {
     response.send("Listagem de usuários")
@@ -13,8 +14,14 @@ router.get("/admin/users/create", (request, response) => {
 router.post("/users/create", (request, response) => {
     const email = request.body.email;
     const password = request.body.password;
+    // hash para salvamento de senhas.
+    const salt = bcryptjs.genSaltSync(10);
+    const hash = bcryptjs.hashSync(password, salt);
 
-    return response.json({email, password});
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => response.redirect("/")).catch((err) => response.redirect("/err"));
 });
 
 module.exports = router;
