65bbafba8b3ff6efec7fdf3ab4850a700ca25e3d
Victor Luiz <victorluizcefet@gmail.com>
2021-05-04 20:20:16 -0300

    wip: Impedindo emails duplicados

--- Blog/user/UsersController.js
+++ Blog/user/UsersController.js
@@ -12,16 +12,27 @@ router.get("/admin/users/create", (request, response) => {
 });
 
 router.post("/users/create", (request, response) => {
+    // fazer validações e etc
     const email = request.body.email;
     const password = request.body.password;
-    // hash para salvamento de senhas.
-    const salt = bcryptjs.genSaltSync(10);
-    const hash = bcryptjs.hashSync(password, salt);
 
-    User.create({
-        email: email,
-        password: hash
-    }).then(() => response.redirect("/")).catch((err) => response.redirect("/err"));
+    User.findOne({where: {email: email}}).then(user => {
+        if(user == undefined){
+             // hash para salvamento de senhas.
+            const salt = bcryptjs.genSaltSync(10);
+            const hash = bcryptjs.hashSync(password, salt);
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => response.redirect("/")).catch((err) => response.redirect("/err"));
+        } else {
+            // colocar uma ideia de email já existente.
+            response.redirect("/admin/users/create")
+        }
+    })
+
+   
 });
 
 module.exports = router;
